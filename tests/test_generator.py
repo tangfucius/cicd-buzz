647e9863c3e4d10fc817fb95ba10061628e5463e
Guofeng Deng <kftang@exciting30.sd.tusimple.ai>
2017-10-10 11:59:40 -0700

    n

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -17,4 +17,4 @@ def test_sample_multiple_words():
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
-    assert len(phrase.split()) >= 5
+    assert len(phrase.split()) >= 6
